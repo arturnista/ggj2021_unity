b2f672c60d0925888ff3ee08f5af338c391d435b
Carlos Alexandre Zgierski <carloszgierski@gmail.com>
2021-01-28 18:38:58 -0300

    Added change weapon mechanic.

--- Assets/_Project/Scripts/Player/PlayerAttack.cs
+++ Assets/_Project/Scripts/Player/PlayerAttack.cs
@@ -6,6 +6,8 @@ public class PlayerAttack : MonoBehaviour
 {
 
     private BaseWeapon _weapon;
+    private BaseWeapon _previousWeapon;
+    [SerializeField] private BaseWeapon[] _weaponList = default;
 
     private void Awake()
     {
@@ -21,6 +23,49 @@ public class PlayerAttack : MonoBehaviour
         else if (Input.GetKeyUp(KeyCode.Mouse0))
         {
             _weapon.StopAttack();
+        }
+        
+        ChangeWeapon();
+    }
+
+    private void ChangeWeapon()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) && _weapon != _weaponList[0])
+        {
+            _previousWeapon = _weapon;
+            _weapon.gameObject.SetActive(false);
+            _weapon = _weaponList[0];
+            _weapon.gameObject.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && _weapon != _weaponList[1])
+        {
+            _previousWeapon = _weapon;
+            _weapon.gameObject.SetActive(false);
+            _weapon = _weaponList[1];
+            _weapon.gameObject.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) && _weapon != _weaponList[2])
+        {
+            _previousWeapon = _weapon;
+            _weapon.gameObject.SetActive(false);
+            _weapon = _weaponList[2];
+            _weapon.gameObject.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) && _weapon != _weaponList[3])
+        {
+            _previousWeapon = _weapon;
+            _weapon.gameObject.SetActive(false);
+            _weapon = _weaponList[3];
+            _weapon.gameObject.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) && _weapon != _previousWeapon && _previousWeapon != null)
+        {
+            var tempPreviousWeapon = _weapon;
+            _weapon.gameObject.SetActive(false);
+            _weapon = _previousWeapon;
+            _previousWeapon = tempPreviousWeapon;
+            _weapon.gameObject.SetActive(true);
         }
     }
 
