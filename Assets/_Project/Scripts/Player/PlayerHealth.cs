4e788a8b4925fe62e946281e7f4b90d9d8e8fbef
Carlos Alexandre Zgierski <carloszgierski@gmail.com>
2021-01-29 17:15:11 -0300

    Damage/Heal feedback

--- Assets/_Project/Scripts/Player/PlayerHealth.cs
+++ Assets/_Project/Scripts/Player/PlayerHealth.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
 
     [SerializeField] private float _maxHealth = 100f;
+    [Header("Feedback")]
+    [SerializeField] private Image _feedbackImage = default;
+
     private float _currentHealth = 100f;
 
     private Animator _animator;
@@ -18,13 +22,25 @@ public class PlayerHealth : MonoBehaviour
 
     public void DealDamage(float damage)
     {
+        StartCoroutine(feedback(Color.red));
         _currentHealth -= damage;
         Debug.LogFormat("Health: {0}", _currentHealth);
     }
 
     public void AddHealth(float health)
     {
+        StartCoroutine(feedback(Color.blue));
         _currentHealth = Mathf.Clamp(0f, _maxHealth, _currentHealth + health);
+    }
+
+    public IEnumerator feedback(Color color)
+    {
+        color.a = 0.5f;
+        _feedbackImage.color = color;
+        _feedbackImage.gameObject.SetActive(true);
+        _feedbackImage.CrossFadeAlpha(0.01f, 0.7f, false);
+        yield return new WaitForSeconds(1.3f);
+        _feedbackImage.gameObject.SetActive(false);
     }
 
 }
