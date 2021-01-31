c3e13c5ec07e06c6c22fff98d8120265f17f9480
Artur Morelle Nista <arturnista@gmail.com>
2021-01-31 15:30:48 -0300

    add main menu img

---
+++ Assets/_Project/Scripts/CaptureScreenshot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureScreenshot : MonoBehaviour
+{
+    
+#if UNITY_EDITOR
+
+    private bool _screenshootActive = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            TakeScreenshot();
+        }
+        else if(Input.GetKeyDown(KeyCode.F2))
+        {
+            if (!_screenshootActive) StartCoroutine(TakeScreenshotCoroutine());
+            else StopAllCoroutines();
+            _screenshootActive = !_screenshootActive;
+        }
+    }
+
+    IEnumerator TakeScreenshotCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f);
+            TakeScreenshot();
+        }
+    }
+
+    void TakeScreenshot()
+    {
+        string filename = "SS_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".png";
+        ScreenCapture.CaptureScreenshot("Images/Screenshots/" + filename);
+        Debug.Log("SS " + filename + " saved");
+    }
+
+#endif
+
+}
